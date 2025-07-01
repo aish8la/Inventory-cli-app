b03be12f84402b777c25655f2ddf86733f2a38df
aish8la <64914670+aish8la@users.noreply.github.com>
2025-07-02 02:17:01 +0500

    feat: add view item function

--- src/item_menu.c
+++ src/item_menu.c
@@ -1,6 +1,9 @@
 #include "item_menu.h"
 #include <stdio.h>
 #include "utilities.h"
+#include "sqlite3.h"
+#include "sqlite_helpers.h"
+#include <stdlib.h>
 
 
 void add_item(void) {
@@ -9,7 +12,32 @@ void add_item(void) {
 }
 
 void view_items(void) {
-    printf("This is the View Item Function");
+
+    sqlite3 *db;
+
+    if(open_db(&db) != 0) {
+        exit(1);
+    }
+
+    sqlite3_stmt *stmt;
+    char *sql = "SELECT item_code, item_name "
+                "FROM items;";
+
+    if(prepare_stmt(db, sql, &stmt) == 1) {
+        return;
+    }
+
+    printf("\n ======== %s ======= \n", "View Item List");
+    printf("\n%-12.10s%-22.20s\n", "Item Code", "Item Name");
+    printf("===================================\n");
+    while (sqlite3_step(stmt) == SQLITE_ROW) {
+        const unsigned char *item_name = sqlite3_column_text(stmt, 1);
+        const unsigned char *item_code = sqlite3_column_text(stmt, 0);
+
+        printf("%-12.10s%-22.20s\n", item_code, item_name);
+
+    }
+
     wait_for_enter();
 }
 
