cf15d3109953919dea2de0888a2bc7d4b1048986
aish8la <64914670+aish8la@users.noreply.github.com>
2025-07-19 00:16:03 +0500

    feat: add table format function

--- include/display_table.h
+++ include/display_table.h
@@ -5,6 +5,11 @@
 
 void print_item_header(void);
 void print_item_row(sqlite3_stmt *stmt, int sn);
+
+void print_stock_header(void);
+void print_stoc_row(sqlite3_stmt *stmt, int sn);
+
+
 int display_table(sqlite3 *db, sqlite3_stmt *stmt, void (*display_header)(void), void (*display_row)(sqlite3_stmt *stmt, int sn));
 
 #endif
--- src/display_table.c
+++ src/display_table.c
@@ -14,6 +14,22 @@ void print_item_row(sqlite3_stmt *stmt, int sn) {
     printf("%-4d%-12.10s%-22.20s\n", sn, item_code, item_name);
 }
 
+//Format to display Item List tables
+void print_stock_header(void) {
+    printf("\n%-4s%-12.10s%-22.20s%-12s%-16s\n", "Sn", "Item Code", "Item Name", "Quantity", "Total Value");
+    printf("===================================\n");
+}
+
+void print_stoc_row(sqlite3_stmt *stmt, int sn) {
+    const char *item_code = (const char *)sqlite3_column_text(stmt, 0);
+    const char *item_name = (const char *)sqlite3_column_text(stmt, 1);
+    int qty = sqlite3_column_int(stmt, 2);
+    double value = sqlite3_column_double(stmt, 3);
+
+
+    printf("%-4s%-12.10s%-22.20s%-12d%-16.2f\n", sn, item_code, item_name, qty, value);
+}
+
 //Function to display table
 int display_table(sqlite3 *db, sqlite3_stmt *stmt, void (*display_header)(void), void (*display_row)(sqlite3_stmt *stmt, int sn)) {
     int rc, found = 0, count = 0;
